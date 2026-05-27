547f226ae93a233063075ac9124af81d17848eee
Paulo Santos <paulo_henrique.st@hotmail.com>
2026-05-27 15:02:28 -0300

    refactor: update ft_memcpy to use pointer arithmetic

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: pauhenr2 <pauhenr2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 09:19:41 by pauhenr2          #+#    #+#             */
-/*   Updated: 2026/05/27 09:51:46 by pauhenr2         ###   ########.fr       */
+/*   Updated: 2026/05/27 10:29:20 by pauhenr2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,20 @@
 
 void	*ft_memcpy(void *dest,const void *src, size_t n)
 {
-	size_t	i;
 	unsigned char *d;
 	const unsigned char *s;
 
-	d = dest;
-	s = src;
-	i = 0;
-	while (i < n)
+	if (dest || src)
 	{
-		d[i] = s[i];
-		i++;
+		d = dest;
+		s = src;
+		while (n > 0)
+		{
+			*d = *s;
+			d++;
+			s++;
+			n--;
+		}
 	}
 	return (dest);
 }
