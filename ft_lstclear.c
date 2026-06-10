331aa5c7aca7cd33b145aed0ea7bbb9bccb47463
Paulo Santos <paulo_henrique.st@hotmail.com>
2026-06-10 18:04:28 -0300

    feat: implement function ft_lstclear

---
+++ ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pauhenr2 <pauhenr2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/10 17:48:16 by pauhenr2          #+#    #+#             */
+/*   Updated: 2026/06/10 18:03:43 by pauhenr2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*next_node;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		next_node = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = next_node;
+	}
+}
